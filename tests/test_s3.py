4bddcb088de63262bcf4c10c1d195df7e918bfd1
danish <danish.saba@gmail.com>
2023-12-28 18:41:08 +0530

    updated tests

--- tests/test_s3.py
+++ tests/test_s3.py
@@ -1,3 +1,4 @@
+import asyncio
 from typing import Any
 
 from srcs.async_s3 import AsyncS3
@@ -5,7 +6,7 @@ from srcs.async_s3 import AsyncS3
 
 class TestS3():
     """AsyncS3 test class"""
-    
+
 
     def test_upload(self, mocker: Any, tmp_path: Any):
         """test async-s3 upload function"""
@@ -15,11 +16,18 @@ class TestS3():
         client = AsyncS3()
         resp = client.upload(str(p), "s3://TEST/test/test.txt")
         assert resp == str(p)
+
+        resp = asyncio.run(mocker.head_object(Bucket="TEST", Key="test/test.txt"))
+        assert resp["ResponseMetadata"]["HTTPStatusCode"] == 200
+        
         
         
     def test_download(self, mocker: Any, tmp_path: Any):
         """test async-s3 upload function"""
         p = tmp_path / "test.txt"
+
+        resp = asyncio.run(mocker.head_object(Bucket="TEST", Key="test/test.txt"))
+        assert resp["ResponseMetadata"]["HTTPStatusCode"] == 200
 
         client = AsyncS3()
         resp = client.download("s3://TEST/test/test.txt", str(p))
